551d9d1c49b3579a345330af4c1e04e9b365255e
Faiz Ur Rehman <faizurrehman1415@gmail.com>
2025-05-09 21:02:51 +0530

    "Hero section demo ready"

--- src/assets/Components/HeroSection/HeroSection.jsx
+++ src/assets/Components/HeroSection/HeroSection.jsx
@@ -3,17 +3,16 @@ import React from 'react'
 function HeroSection() {
   return (
     <>
-         <div className='bg-gradient-to-r from-amber-50 to-[#faebd7] rounded-3xl w-[85vw] h-[70vh] mx-auto'>
-           <div className='p-10 flex gap-40'>
+         <div className='bg-gradient-to-r from-amber-50 to-[rgb(250,235,215)] rounded-3xl w-[85vw] h-[70vh] mx-auto'>
+           <div className='p-10 flex gap-30'>
                 <div className='w-[40vw] mt-20 '>
                     <h1 className='text-5xl font-bold text-yellow-700 mb-5'>Welcome to Tabib Care</h1>
-                    <h2 className='text-2xl font-semi-bold mb-5'>Natural Wellness, Timeless Benefits</h2>
+                    <h2 className='text-2xl font-semi-bold mb-5'>Natural Wellness, Timeless Benefits.</h2>
                     <p className="font-semi-bold text-[#aaa0a0]">Empowering you with high-quality herbal products inspired by Unani, Ayurveda, and nature.</p>
                 </div>
-                <div className=''>
-                    {/* <video  autoPlay loop muted  className='ani-hero w-[20vw] rounded-3xl' src="img/Hero-video.mp4"></video> */}
-                   <img className='w-[20vw] ani-hero' src="img/hero-img.png" alt="" />
-                   <h2 className='text-center text-3xl font-bold text-yellow-700 ani-hero-p'>Tibb Hair Oil</h2>
+                <div className='relative mt-10'>
+                   <img className='absolute left-16 top-28 w-[7.5vw] ani-hero' src="img/hero-img.png" alt="" />
+                    <img className='w-[40vw] ani-hero-person' src="img/her0-1.png" alt="" />
                 </div>
 
            </div>
